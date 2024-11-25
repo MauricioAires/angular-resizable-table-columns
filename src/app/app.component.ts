7ed61338b6393b94241e8da9530c3c8f4f5efae6
MauricioAires <mauricioaires015@gmail.com>
2024-11-25 09:18:49 -0300

    feat: create example with resizable directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { ResizableComponent } from './components/resizable/resizable.component';
+import { ResizableDirective } from './components/resizable/resizable.directive';
+
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ResizableComponent, ResizableDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'angular-resizable-table-columns';
+export class AppComponent implements OnInit {
+  protected rows = signal<any[]>([]);
+
+  public ngOnInit(): void {
+    const data = Array.from(Array(10)).map(() => {
+      return [
+        faker.database.mongodbObjectId(),
+        faker.person.fullName(),
+        faker.location.streetAddress(),
+        faker.location.city(),
+        faker.location.state(),
+        faker.location.zipCode(),
+      ];
+    });
+
+    this.rows.set(data);
+  }
 }
